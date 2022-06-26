b5eda93af3b42dfc14b5c68e03b0c9240bfa0c53
hymsoft <hymsoft@gmail.com>
2022-06-26 17:40:14 -0300

    Creado los componentes principales y movido el html y css a cada componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SocialBarComponent } from './componentes/social-bar/social-bar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    SocialBarComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule
