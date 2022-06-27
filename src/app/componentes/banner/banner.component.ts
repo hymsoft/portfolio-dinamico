92116ad7756ff66e0cbef7421f925ea58da4c3c1
hymsoft <hymsoft@gmail.com>
2022-06-27 07:37:53 -0300

    Componente banner obtiene los datos de un objeto perfil

--- src/app/componentes/banner/banner.component.ts
+++ src/app/componentes/banner/banner.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent implements OnInit {
+  perfil = {
+    nombre: 'Hugo Antonio Segura',
+    titulo: 'Full Stack Developer Jr.',
+    slogan: 'while(!(succeed = try()));',
+    imagen: '../assets/img/profile.jpg',
+    imagenAlt: 'Foto perfil Segura Hugo Antonio',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-encabezado',
   templateUrl: './encabezado.component.html',
-  styleUrls: ['./encabezado.component.css']
+  styleUrls: ['./encabezado.component.css'],
 })
 export class EncabezadoComponent implements OnInit {
-  logoArgentinaPrograma = "../assets/img/APLogo-20-20.png";
+  logoArgentinaPrograma = '../assets/img/APLogo-20-20.png';
+  logoArgentinaProgramaAlt = 'Logo Plan Argentina Programa';
+  brand = '#YoProgramo';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
