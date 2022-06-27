bcd1d33106715282708c1f2f3e98da681c23a072
hymsoft <hymsoft@gmail.com>
2022-06-27 10:39:58 -0300

    Modificado el componente sobre mi para recibir datos dinamicos

--- README.md
+++ README.md
@@ -11,13 +11,15 @@ Ejecute `ng serve` para un servidor de desarrollo. Navegue a `http://localhost:4
 
 Ejecute `ng build` para compilar el proyecto. Los archivos de compilación se almacenarán en el directorio `dist/`.
 
-## Hoja de Ruta  
-**26/06/2022** 
-+ Creación del proyecto Angular y traspaso del portfolio estatico al componente principal.  
-+ Creación de los componentes base y traspaso del html y css a su correspondiente componente.
+## Hoja de Ruta
 
-**27/06/2022** 
-+ El componente encabezado recibe los datos a traves de `property binding` e `interpolación`  
-+ El componente banner recibe los datos desde un objeto `perfil`  
+**26/06/2022**
 
+- Creación del proyecto Angular y traspaso del portfolio estatico al componente principal.
+- Creación de los componentes base y traspaso del html y css a su correspondiente componente.
 
+**27/06/2022**
+
+- El componente encabezado recibe los datos a traves de `property binding` e `interpolación`
+- El componente banner recibe los datos desde un objeto `perfil`
+- El componente sobreMi recibe los datos desde un objeto `sobreMi`
--- src/app/componentes/sobre-mi/sobre-mi.component.ts
+++ src/app/componentes/sobre-mi/sobre-mi.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sobre-mi',
   templateUrl: './sobre-mi.component.html',
-  styleUrls: ['./sobre-mi.component.css']
+  styleUrls: ['./sobre-mi.component.css'],
 })
 export class SobreMiComponent implements OnInit {
+  sobreMi = {
+    titulo: 'Hola, soy @hymsoft',
+    texto: [
+      'Un antiguo programador. Empecé con dbase, Clipper, FoxPro for D.O.S., Visual FoxPro, basic, visual basic... Apasionado por la tecnología, siempre estudiando y aprendiendo. Usuario avanzado de Excel, ferviente eliminador de virus informáticos sin pérdida de datos. Amante del backend, enamorándome del FrontEnd.',
+      'Actualmente aprendiendo: Programación web con HTML-CSS-JS Curso de C# en la UTN-FRT - Universidad Tecnológica Nacional - Facultad Regional Tucumán - Profesora: Escobar Estela Mabel Segunda etapa del plan #ProgramaArgentina - Full Stack Developer - Profesora: Karin Fleischer.',
+      'Me gustan los deportes, especialmente rugby. Hincha de Boca Juniors, Club Atético Talleres de Córdoba y Huirapuca R.C',
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
